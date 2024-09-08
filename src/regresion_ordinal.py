28a3d3f1921a95396463e8e8a06f178e6962eaa9
carl0sArauj0 <165329168+carl0sArauj0@users.noreply.github.com>
2024-09-08 18:47:12 -0500

    Update regresion_ordinal.py

--- src/regresion_ordinal.py
+++ src/regresion_ordinal.py
@@ -1,11 +1,31 @@
 import statsmodels.api as sm
-import statsmodels.formula.api as smf
+from sklearn.metrics import accuracy_score
+import pandas as pd
 
-def regresion_ordinal(df_performance):
-    model = smf.ols('Cumulative_GPA ~ C(Student_Age) + C(Additional_work) + C(Graduated_high_school_type)', data=df_performance).fit()
+def regression_logistic_ordinal(df, target_variable, predictor_variables):
     
-    print("Resultados Regresión Ordinal: ")
-    print(model.summary())
+    for variable in predictor_variables:
+        df[variable] = df[variable].astype('category')
+        
+    y = df[target_variable] 
+    X = df[predictor_variables]
     
-    return model
+    X = sm.add_constant(X)
+
+    model_ordinal = sm.MNLogit(y, X).fit()
+
+    print(model_ordinal.summary())
+    
+    predicciones = model_ordinal.predict(X)
+    predicciones_clases = predicciones.idxmax(axis=1)  
+    
+    accuracy = accuracy_score(y, predicciones_clases)
+    print(f"Accuracy del modelo: {accuracy:.4f}")
+
+    return accuracy
+
+predictor_variables = ['Student_Age', 'Additional_work', 'Graduated_high_school_type']
+target_variable = 'Cumulative_GPA'
+
+accuracy = regression_logistic_ordinal(df, target_variable, predictor_variables)
 
